14d8d8ea2e1468df92d7bf354f75e0a65ec3e9be
Alba Pérez César <alba.perez.ce@gmail.com>
2021-02-25 16:30:35 +0100

    añadida funcion de agregar vehiculos

--- Concesionario_MVC/WS_Soap.asmx.cs
+++ Concesionario_MVC/WS_Soap.asmx.cs
@@ -20,7 +20,7 @@ namespace Concesionario_MVC
     // [System.Web.Script.Services.ScriptService]
     public class WS_Soap : System.Web.Services.WebService
     {
-
+        AdventureWorksLT2017Entities db = new AdventureWorksLT2017Entities();
 
         [WebMethod]
         public string HelloWorld()
@@ -33,10 +33,48 @@ namespace Concesionario_MVC
         {
             Vehiculos vehs = new Vehiculos();
 
-                vehs.marca = "Dacia";
-                vehs.color = "Verde";
-
+            vehs.marca = "Dacia";
+            vehs.color = "Verde";
             return vehs;
         }
+
+        [WebMethod]
+        public Vehiculos AddVehiculosXML(string marca, int numeroPuertas, string color, long km, string tipoVeh, int garantia,
+            bool enStock, bool fotografia, string modelo) //Para introducir los datos desde el proyecto de consumidor
+        {
+            using (db)
+            {
+                
+                Vehiculos veh = new Vehiculos();
+                /*
+                 * // Con datos preinsertados
+                veh.marca = "Kia";
+                veh.numeroPuertas = 5;
+                veh.color = "Rosa";
+                veh.kilometros = 166666;
+                veh.tipoVehiculo = "SUV";
+                veh.garantia = 27;
+                veh.enStock = true;
+                veh.fotografia = false;
+                veh.modelo = "Nadal";
+                */
+
+                // Con datos ya introducidos desde el propio consumidor
+                veh.marca = marca;
+                veh.numeroPuertas = numeroPuertas;
+                veh.color = color;
+                veh.kilometros = km;
+                veh.tipoVehiculo = tipoVeh;
+                veh.garantia = garantia;
+                veh.enStock = enStock;
+                veh.fotografia = fotografia;
+                veh.modelo = modelo;
+
+                db.Vehiculos.Add(veh);
+                db.SaveChanges();
+                return veh;
+            }
+        }
+
     }
 }
